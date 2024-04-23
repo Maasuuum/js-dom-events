cd46bcc1cc0f07fbd035f329c616a12efd711b38
Maasuuum <fardousalmasum09@gmail.com>
2024-04-23 16:19:26 +0600

    recap two

--- js/index.js
+++ js/index.js
@@ -24,3 +24,43 @@
 //     let iitemAll = li[iitemAll];
 //     iitemAll.style.color = "red";
 // }
+
+
+// const parent = document.querySelector(".grandparent");
+// console.log(parent.children[0]);
+
+// const gp = document.querySelector(".grandparent");
+// const children = gp.querySelectorAll(".item");
+// console.log(children);
+
+// const children = document.querySelector(".item");
+// const parent = children.parentElement.parentElement;
+// console.log(parent);
+
+// const children = document.querySelector(".item");
+// const gp = children.closest(".grandparent");
+// console.log(gp);
+
+// const chacha = document.getElementById("parent-two");
+// console.log(chacha.previousElementSibling);
+
+// const h3 = document.createElement("h3");
+// h3.innerText = "made by js";
+
+// const container = document.querySelector(".grandparent");
+// container.appendChild(h3);
+// console.log(h3);
+
+// const header = document.createElement("h2");
+// header.innerText = "Made by JS";
+
+const container = document.querySelector(".grandparent");
+const newDiv = document.createElement("div");
+newDiv.innerHTML = `
+    <h1>Hello World</h1>
+    <h2>How Are You?</h2>
+`
+// container.appendChild(newDiv);
+let h1 = document.getElementById("parent-one");
+
+container.insertBefore(newDiv.h1);
