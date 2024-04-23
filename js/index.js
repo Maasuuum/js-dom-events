454c523218e8572dfc5a541f4276a0acc93759b1
Maasuuum <fardousalmasum09@gmail.com>
2024-04-23 10:37:59 +0600

    recap

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// const head = document.getElementById('parent');
+// console.log(head);
+
+// const classDom = document.getElementsByClassName("item");
+// // console.log(classDom);
+// for (let item of classDom){
+//     item.style.color = "green"; 
+// }
+
+// const myTags = document.getElementsByTagName("li");
+// // console.log(myTags);
+// for(let i = 0; i < myTags.length; i++){
+//     // console.log(myTags[i]);
+//     let element = myTags[i];
+//     element.style.color = "blue";
+// }
+
+// const h1 = document.querySelector("#list-item");
+// console.log(h1);
+
+// const li = document.querySelectorAll(".item");
+// // console.log(li);
+// for(let i = 0; i < li.length; i++){
+//     let iitemAll = li[iitemAll];
+//     iitemAll.style.color = "red";
+// }
